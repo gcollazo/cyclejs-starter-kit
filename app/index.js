94b7ada8e20e99c1c4cd5886f05e47667d29ae6f
Giovanni Collazo <hello@gcollazo.com>
2016-01-23 10:37:00 -0400

    Removes div from initial message

--- app/index.js
+++ app/index.js
@@ -1,13 +1,11 @@
 import Rx from 'rx';
 import { run } from '@cycle/core';
-import { makeDOMDriver, div, h1 } from '@cycle/dom';
+import { makeDOMDriver, h1 } from '@cycle/dom';
 
 function main() {
   return {
     DOM: Rx.Observable.of(
-      div([
-        h1('Welcome to Cycle.js')
-      ])
+      h1('Welcome to Cycle.js')
     )
   };
 }
